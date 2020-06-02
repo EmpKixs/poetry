db03d5752cfb51756fadbde1959181920e1f9237
kixs <emp_kixs@163.com>
2020-06-02 08:48:29 +0800

    fix

---
+++ src/main/java/com/kixs/poetry/PoetryApplication.java
@@ -0,0 +1,13 @@
+package com.kixs.poetry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoetryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoetryApplication.class, args);
+    }
+
+}
