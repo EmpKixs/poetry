be44ecbce910bd16eb86df1a046f19e9332487f4
syx_ouyuchen <3143035724@qq.com>
2020-10-06 22:03:43 +0800

    fix

--- src/main/java/com/kixs/poetry/parser/ParserSupport.java
+++ src/main/java/com/kixs/poetry/parser/ParserSupport.java
@@ -1,5 +1,8 @@
 package com.kixs.poetry.parser;
 
+import com.hankcs.hanlp.HanLP;
+import com.kixs.poetry.entity.Author;
+import com.kixs.poetry.entity.Poetry;
 import com.kixs.poetry.enums.ParserEnum;
 import com.kixs.poetry.service.AuthorService;
 import com.kixs.poetry.service.PoetryService;
@@ -13,6 +16,8 @@ import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -25,8 +30,6 @@ import java.util.Map;
 @Slf4j
 @Component
 public class ParserSupport {
-
-    private ZHConverter converter = ZHConverter.getInstance(ZHConverter.SIMPLIFIED);
 
     @Resource
     private Map<String, PoetryParser> poetryParserMap;
@@ -55,20 +58,53 @@ public class ParserSupport {
         }
         log.debug("解析数据：作者-{}，诗词-{}", context.getAuthorMap().size(), context.getPoetries().size());
         if (!CollectionUtils.isEmpty(context.getPoetries())) {
-            context.getPoetries().forEach(poetry -> {
+            context.getPoetries().parallelStream().forEach(poetry -> {
                 poetry.setTitle(convert(EmojiUtils.replaceEmoji(poetry.getTitle())));
                 poetry.setContent(convert(EmojiUtils.replaceEmoji(poetry.getContent())));
                 poetry.setRhythmic(convert(EmojiUtils.replaceEmoji(poetry.getRhythmic())));
+                // poetryService.insert(poetry);
             });
-            poetryService.insertBatch(context.getPoetries());
+            List<List<Poetry>> data = new ArrayList<>();
+            int index = 0;
+            int step = 1000;
+            int length = context.getPoetries().size();
+            while (index <= length) {
+                int toIndex = index + step;
+                if (toIndex >= length) {
+                    data.add(context.getPoetries().subList(index, length));
+                    // poetryService.insertBatch(context.getPoetries().subList(index, length));
+                } else {
+                    data.add(context.getPoetries().subList(index, toIndex));
+                    // poetryService.insertBatch(context.getPoetries().subList(index, toIndex));
+                }
+                index = toIndex;
+            }
+            data.parallelStream().forEach(poetries -> poetryService.insertBatch(poetries));
         }
         if (!CollectionUtils.isEmpty(context.getAuthorMap())) {
-            context.getAuthorMap().values().forEach(author -> {
+            context.getAuthorMap().values().parallelStream().forEach(author -> {
                 author.setName(convert(EmojiUtils.replaceEmoji(author.getName())));
                 author.setDescription(convert(EmojiUtils.replaceEmoji(author.getDescription())));
                 author.setShortDescription(convert(EmojiUtils.replaceEmoji(author.getShortDescription())));
+                authorService.insert(author);
             });
-            authorService.insertBatch(context.getAuthorMap().values());
+            // authorService.insertBatch(context.getAuthorMap().values());
+            List<List<Author>> data = new ArrayList<>();
+            int index = 0;
+            int step = 1000;
+            int length = context.getPoetries().size();
+            while (index <= length) {
+                int toIndex = index + step;
+                if (toIndex >= length) {
+                    data.add(context.getAuthorMap().values().subList(index, length));
+                    // poetryService.insertBatch(context.getPoetries().subList(index, length));
+                } else {
+                    data.add(context.getPoetries().subList(index, toIndex));
+                    // poetryService.insertBatch(context.getPoetries().subList(index, toIndex));
+                }
+                index = toIndex;
+            }
+            data.parallelStream().forEach(poetries -> poetryService.insertBatch(poetries));
         }
         return context;
     }
@@ -77,12 +113,13 @@ public class ParserSupport {
         if (StringUtils.isEmpty(source)) {
             return source;
         }
-        return converter.convert(source);
+        return HanLP.convertToSimplifiedChinese(source);
     }
 
     public static void main(String[] args) {
         String data = "靈山話月";
         ZHConverter converter = ZHConverter.getInstance(ZHConverter.SIMPLIFIED);
         System.out.println(converter.convert(data));
+        System.out.println(HanLP.convertToSimplifiedChinese(data));
     }
 }
