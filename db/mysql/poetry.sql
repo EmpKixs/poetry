0c8c50d4bac7af54f9497f0817e8e727abc92a42
kixs <emp_kixs@163.com>
2020-10-06 22:05:55 +0800

    fix

--- db/mysql/poetry.sql
+++ db/mysql/poetry.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50727
  File Encoding         : 65001
 
- Date: 16/08/2020 23:17:14
+ Date: 06/10/2020 22:05:17
 */
 
 SET NAMES utf8mb4;
@@ -23,10 +23,10 @@ SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS `author`;
 CREATE TABLE `author`  (
   `id` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '主键ID',
-  `name` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '姓名',
-  `dynasty` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '朝代',
-  `description` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '生平简介',
-  `short_description` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '生平简介（短）',
+  `name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '姓名',
+  `dynasty` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '朝代',
+  `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL COMMENT '生平简介',
+  `short_description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL COMMENT '生平简介（短）',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '诗词经典（历史子集）作者' ROW_FORMAT = Dynamic;
 
@@ -36,11 +36,12 @@ CREATE TABLE `author`  (
 DROP TABLE IF EXISTS `poetry`;
 CREATE TABLE `poetry`  (
   `id` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '主键ID',
-  `type` tinyint(3) NOT NULL COMMENT '诗词类型',
-  `title` varchar(120) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '标题',
-  `author_id` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '作者',
-  `content` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '内容',
-  `notes` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '注释',
+  `type` tinyint(3) NULL DEFAULT NULL COMMENT '诗词类型',
+  `title` varchar(520) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '标题',
+  `rhythmic` varchar(520) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '词牌名',
+  `author_id` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '作者',
+  `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '内容',
+  `notes` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL COMMENT '注释',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '诗歌' ROW_FORMAT = Dynamic;
 
