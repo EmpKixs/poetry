4a495ec3d64448f79f80de1f65874ce654e29c2e
kixs <emp_kixs@163.com>
2021-07-22 18:27:55 +0800

    典籍解析SQL

--- db/mysql/poetry.sql
+++ db/mysql/poetry.sql
@@ -55,4 +55,37 @@ CREATE TABLE `poetry`  (
    INDEX `idx_author_id`(`author_id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '诗歌' ROW_FORMAT = Dynamic;
 
+-- ----------------------------
+-- Table structure for article
+-- ----------------------------
+DROP TABLE IF EXISTS `article`;
+CREATE TABLE `article`  (
+    `id` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '主键ID',
+    `book_id` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '典籍ID',
+    `volume` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '卷名',
+    `volume_sort` int(11) NULL DEFAULT NULL COMMENT '卷排序',
+    `section` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '章节/篇章',
+    `section_sort` int(11) NULL DEFAULT NULL COMMENT '章节/篇章排序',
+    `title` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '标题',
+    `sort` int(11) NULL DEFAULT NULL COMMENT '排序',
+    `author_id` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '内容作者ID',
+    `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '内容',
+    `notes` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL COMMENT '注释',
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '古代典籍内容' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for book
+-- ----------------------------
+DROP TABLE IF EXISTS `book`;
+CREATE TABLE `book`  (
+    `id` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '主键ID',
+    `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '典籍名称',
+    `author_id` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '作者ID',
+    `introduction` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL COMMENT '典籍介绍',
+    `tag` varchar(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '典籍标签',
+    PRIMARY KEY (`id`) USING BTREE,
+    INDEX `idx_author_id`(`author_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '古代典籍' ROW_FORMAT = Dynamic;
+
 SET FOREIGN_KEY_CHECKS = 1;
